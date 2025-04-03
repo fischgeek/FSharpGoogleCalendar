68e5498064c5e208c6695e4454cbc4ff1e5c88b1
fischgeek <fischgeek@gmail.com>
2025-04-03 18:38:32 -0500

    quick

--- FSharpGoogleCalendar/GoogleCalendar.fs
+++ FSharpGoogleCalendar/GoogleCalendar.fs
@@ -10,7 +10,7 @@ open System.Threading
 
 module GoogleCalendar =
 
-    let private scopes = [| CalendarService.Scope.CalendarReadonly |]
+    let private scopes = [| CalendarService.Scope.CalendarReadonly; CalendarService.Scope.CalendarEvents |]
 
     // https://developers.google.com/calendar/quickstart/dotnet
     let private credsFile = @"c:\dev\config\google-calendar-credentials.json"
@@ -20,7 +20,7 @@ module GoogleCalendar =
         let stream = new FileStream(credsFile, FileMode.Open, FileAccess.Read)
         let credential =
             (GoogleWebAuthorizationBroker.AuthorizeAsync
-                (GoogleClientSecrets.Load(stream).Secrets, scopes, "user", CancellationToken.None, new FileDataStore(credPath, true))).Result
+                (GoogleClientSecrets.FromStream(stream).Secrets, scopes, "user", CancellationToken.None, new FileDataStore(credPath, true))).Result
         credential
 
     let private CreateGoogleCalendarService credential =
@@ -29,15 +29,34 @@ module GoogleCalendar =
         baseClientService.ApplicationName <- "Application Name"
         new CalendarService(baseClientService)
 
-    let private CreateRequest minDate maxDate service =
-        let request = new EventsResource.ListRequest(service, "primary")
+    let private CreateRequest calId minDate maxDate service =
+        let request = new EventsResource.ListRequest(service, calId)
         request.TimeMin <- Nullable minDate
         request.TimeMax <- Nullable maxDate
         request.ShowDeleted <- Nullable false
         request.SingleEvents <- Nullable true
-        request.MaxResults <- Nullable 10
+        //request.MaxResults <- Nullable 10
         request.OrderBy <- Nullable EventsResource.ListRequest.OrderByEnum.StartTime
         request
+
+    let private createUpdateRequest calId event colorId service = 
+        let request = new EventsResource.UpdateRequest(service, event, calId, event.Id)
+        request
+
+    let GetAllCalendars () =
+        printfn "All Calendars"
+        let service = Authenticate() |> CreateGoogleCalendarService
+        let myCals = service.CalendarList.List().Execute()
+        myCals.Items
+        |> Seq.toList
+        |> Seq.iter (fun cal -> 
+            printfn $"-{cal.Summary}"
+            printfn $"\tId: {cal.Id}"
+            printfn $"\tSummary: {cal.Summary}"
+            printfn $"\tSummaryO: {cal.SummaryOverride}"
+            printfn $"\tDescription: {cal.Description}"
+            printfn $"\tAccess Role: {cal.AccessRole}"
+        )
 
     let private GetEvents(request: EventsResource.ListRequest) =
         try
@@ -47,13 +66,41 @@ module GoogleCalendar =
             |> function
             | [] -> None
             | list -> Some list
-        with ex -> failwith ex.Message
+        with ex -> 
+            printfn $"Calendar not found: {request.CalendarId}"
+            None
 
-    let GetEventList() =
-        let startDate = DateTime.Now
-        let endDate = DateTime.Now
+    let GetEventList calId startDate endDate =
         Authenticate()
         |> CreateGoogleCalendarService
-        |> CreateRequest startDate endDate
+        |> CreateRequest calId startDate endDate
         |> GetEvents
-    ()
+
+    let UpdateEventColor calId eventId colorId = 
+        let req = 
+            Authenticate()
+            |> CreateGoogleCalendarService
+            |> createUpdateRequest calId eventId colorId
+        try
+            req.Execute()
+        with ex -> failwith ex.Message
+
+    let GetCalendarColor calId = 
+        let service = 
+            Authenticate() 
+            |> CreateGoogleCalendarService
+        try
+            let myCal = service.CalendarList.Get(calId).Execute()
+            Some (myCal.BackgroundColor,myCal.ForegroundColor)
+        with ex -> None
+        
+
+    let GetColors colorId =
+        let service = Authenticate() |> CreateGoogleCalendarService
+        let clrs = service.Colors.Get().Execute()
+        let dictionary = clrs.Calendar
+        let c = dictionary.Item(colorId)
+        //printfn $"ETag: {c.ETag}"
+        //printfn $"FG: {c.Foreground}"
+        //printfn $"BG: {c.Background}"
+        c
--- FSharpGoogleCalendarTest/Program.fs
+++ FSharpGoogleCalendarTest/Program.fs
@@ -5,18 +5,40 @@ open System
 
 module Main =
     [<EntryPoint>]
-    let main argv =
-        GoogleCalendar.GetEventList()
+    let main argv =      
+        let calendarId = "8vr7je3qbbcaep27ccjf4kbjng@group.calendar.google.com"
+        let startDate = DateTime.Now
+        let endDate = DateTime.Now.AddDays(2.)
+        
+        let firstGroup() = 
+            GoogleCalendar.GetEventList calendarId startDate endDate
+            |> function
+            | Some list -> 
+                list 
+                |> Seq.iter (fun event -> 
+                    printfn "--------------------------------------------------"
+                    printfn "%s [%A]" event.Summary event.Start.DateTime.Value
+                    printfn "%s" event.Location
+                )
+            | None -> 
+                printfn "No events."
+                ()
+
+        let events = GoogleCalendar.GetEventList "fischious@gmail.com" startDate endDate
+
+        events
         |> function
         | Some list -> 
             list 
             |> Seq.iter (fun event -> 
                 printfn "--------------------------------------------------"
-                printfn "%s [%A]" event.Summary event.Start.DateTime.Value
+                printfn "%s [%A]" event.Summary event.Start.DateTimeRaw
                 printfn "%s" event.Location
             )
         | None -> 
             printfn "No events."
             ()
+        //let bg,fg = GoogleCalendar.GetCalendarColor "primary"
+        GoogleCalendar.GetAllCalendars()
         Console.ReadLine() |> ignore
         0
